f4054d301577b5d16dbb403bc37910897a6c8cde
Stephulz <stefansanches9@gmail.com>
2021-04-06 11:30:42 -0300

    Jotto app, Congrats component test

---
+++ jotto/src/Congrats.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+/**
+ * Functional react component for congratulatory message.
+ * @param {object} success - Boolean  
+ * @returns {JSX.Element} - Rendered component (or null if `success` prop is false)
+ */
+export default function Congrats({ success }) {
+    if (success) {
+        return (
+            <div data-test="component-congrats">
+                <span data-test="congrats-message">
+                    Congratulations! You guessed the word!
+                </span>
+            </div>
+        )
+    } else {
+        return (
+            <div data-test="component-congrats" />
+        )
+    }
+}
+
+Congrats.propTypes = {
+    success: PropTypes.bool.isRequired
+}
