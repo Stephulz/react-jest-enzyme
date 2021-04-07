7fe5af6e06d734987d1fd9ef3c35eae8da3b893e
Stephulz <stefansanches9@gmail.com>
2021-04-07 09:00:05 -0300

    GuessedWords component test, propTypes test, describe, beforeEach, setupTests, testUtils

--- jotto/src/App.test.js
+++ jotto/src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { shallow } from 'enzyme';
+import { findByTestAttr } from '../test/testUtils';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const setup = () => {
+  return shallow(<App />)
+};
+
+test('renders without error', () => {
+  let wrapper = setup();
+  const component = findByTestAttr(wrapper, "main-container");
+  expect(component.length).toBe(1);
 });
